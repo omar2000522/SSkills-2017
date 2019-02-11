8f5b2ac1a18a051929ae0ad9ddae9a38c9c6c228
omar2_000 <omar2000522@gmail.com>
2019-02-11 15:01:01 +0300

    Added md5 hashing feature

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,50 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        BorderPane root = new BorderPane();
+        TextField inputField = new TextField();
+        Label outputLabel = new Label();
+        Button hashButton = new Button("Hash");
+        VBox midBox = new VBox(inputField,outputLabel,hashButton);
+        midBox.setAlignment(Pos.CENTER);
+        midBox.setSpacing(20);
+        midBox.setPadding(new Insets(30));
+        root.setCenter(midBox);
+
+        hashButton.setOnAction(val -> {
+            try {
+                MessageDigest md = MessageDigest.getInstance("md5");
+                byte[] digest = md.digest(inputField.getText().getBytes());
+                outputLabel.setText(DatatypeConverter.printHexBinary(digest));
+
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 
